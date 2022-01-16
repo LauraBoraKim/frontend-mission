5973e757e4cad385a0563b9f0d1d8053c2574676
Bora Kim <zh2464@naver.com>
2022-01-16 21:30:07 +0900

    feat: 상품 정보 더미데이터 생성

---
+++ mission/src/data/itemInfo.js
@@ -0,0 +1,30 @@
+const ITEMINFOS = [
+  {
+    id: 1,
+    seller: {
+      id: 1,
+      tag: '#귀걸이 #목걸이 #반지 #피어싱 #악세사리',
+      sellerName: '블링살롱',
+      sellerImageUrl: 'https://image.brandi.me/seller/soun91_profile_1618918108.jpg',
+      storeLikeCount: 9725,
+      satisfaction: 96,
+    },
+    mainImageUrl:
+      'https://user-images.githubusercontent.com/50609368/149651032-56f84c6f-ed6c-4df3-8805-5f852559d4f3.png',
+    title: '프렌치 투웨이 볼드 링 진주 귀걸이',
+    itemDetail:
+      '<img src="https://image.brandi.me/cproductdetail/2022/01/16/d4b5adcffbb4d944d7e9c9df636927de.jpg" alt="상품상세이미지"  style="width:100%; height:100%;"/><div>볼드한 오픈링에 언발런스한 침 디자인으로</br> 제작된 귀걸이입니다. </br>흔하지않는 유니크한 디자인의 제품으로</br>엣지있는 세련된 포인트가 되는 귀걸이 입니다.</br></br>MATERIAL_</br>Brass + 알러지방지도금</br>COLOR_</br>Gold , Silver</div> <img src="https://image.brandi.me/cproductdetail/2022/01/16/a9f112e78292ba6a39d8c0448a0408c9.jpg" alt="상품상세이미지"  style="width:100%; height:100%;"/>',
+    isStoreLiked: true, // 상품 좋아요 정보는 원래 유저 정보로부터 가져오지만, 이번 미션에서 유저 데이터를 따로 사용하지 않으므로 상품데이터에 포함시켰다.
+    isItemLiked: false,
+    likeCount: 1205,
+    isDiscount: true,
+    price: 15000,
+    discountRate: 20,
+    discountPrice: 12000,
+    purchasePrice: 12000,
+    createdDate: '2022-01-15T18:33:21',
+    modifiedDate: '2021-01-16T16:24:40',
+  },
+];
+
+export default ITEMINFOS;

588e6002f0597ecc4682fb54a8babd0e349a52e5
Bora Kim <zh2464@naver.com>
2022-01-16 21:25:47 +0900

    feat: 라우터 홈 컴포넌트 변경

--- mission/src/router/index.js
+++ mission/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import ItemDetails from '@/views/ItemDetails.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: ItemDetails,
   },
   {
     path: '/about',

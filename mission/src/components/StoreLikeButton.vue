4bf5c2b5535e2250c2180acc1221085d2943d272
Bora Kim <zh2464@naver.com>
2022-01-16 21:27:26 +0900

    feat: 스토어 좋아요 기능을 위한 컴포넌트 생성

---
+++ mission/src/components/StoreLikeButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="this.isLiked === true">
+    <img class="img-storeLike" src="../assets/storeLikeIcon.svg" alt="" @click="onLikeClick" />
+  </div>
+  <div v-else>
+    <img class="img-storeLike" src="../assets/storeLikedIcon.svg" alt="" @click="onLikeClick" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'storeLike',
+  props: {
+    like: Boolean,
+  },
+  data() {
+    return {
+      isLiked: this.like,
+    };
+  },
+
+  methods: {
+    onLikeClick() {
+      this.isLiked = !this.isLiked;
+    },
+  },
+};
+</script>
+<style lang="scss">
+@import '../assets/style/StoreLikeButton.scss';
+</style>
